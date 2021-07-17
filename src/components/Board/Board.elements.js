537c8cf847b62b6b0e554e56e0a4b5ce074cb829
givamarques <49195218+givamarques@users.noreply.github.com>
2021-07-17 17:18:56 -0300

    estilo ok

--- src/components/Board/Board.elements.js
+++ src/components/Board/Board.elements.js
@@ -1,15 +1,121 @@
 import styled from 'styled-components'
 
-export const Square = styled.div`
-background: lightgray;
+const Square = styled.div` //aqui o quadro principal foi chamado de Square, ao ser importado no "Board.js" atraves de S(ponto) todos os atributos serao aplicados
+background: #dfdfdf;
 height: 350px;
 width: 900px;
 border-radius: 25px;
 display: flex;
 justify-content: center;
+align-items: stretch;
+font-size: 1.2rem;
+position: static;
+`
+
+const Input = styled.input`
+background: white;
+height: 35px;
+width: 450px;
+border-color: #c7bdbd;
+border-radius: 10px;
+display: inline-block;
+margin-bottom: 15px;
+justify-content: center;
 align-items: center;
 font-size: 1.2rem;
 position: static;
-top: 0;
-z-index: 999;
+::placeholder{
+    padding-left: 15px;
+    color:#c7bdbd;
+}
 `
+
+const InputAdress = styled.input`
+background: white;
+height: 35px;
+width: 450px;
+border-color: #c7bdbd;
+border-radius: 10px;
+display: inline-block;
+margin-left: 15px;
+margin-right: 15px;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: static;
+::placeholder{ //aqui é possivel passar um atributo dentro de outro atributo. No caso, o placeholder dentro do input
+    padding-left: 10px;
+    color:black;
+}
+`
+
+const Neighborhood = styled.input`
+background: white;
+height: 35px;
+width: 300px;
+border-color: #c7bdbd;
+border-radius: 10px;
+display: inline-block;
+margin-left: 15px;
+margin-right: 15px;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: static;
+::placeholder{
+    padding-left: 10px;
+    color:black;
+}
+`
+
+const UF = styled.input`
+background: white;
+height: 35px;
+width: 85px;
+border-color: #c7bdbd;
+border-radius: 10px;
+display: inline-block;
+margin-left: 15px;
+margin-right: 15px;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: static;
+::placeholder{
+    padding-left: 10px;
+    color:black;
+}
+`
+
+const Button = styled.button`
+background: #18ac00;
+color: white;
+border: 0;
+height: 35px;
+width: 120px;
+border-radius: 10px;
+display: inline-block;
+margin-left: 20px;
+justify-content: center;
+align-items: center;
+font-size: 1.2rem;
+position: static;
+`
+
+const Title = styled.p`
+font-size: 22;
+font-family: Arial, Helvetica, sans-serif;
+color: gray;
+margin-left: 15px;
+margin-right: 15px;
+`
+
+export const S ={ //ao inves de exportar separadamente pode-se reunir os diferentes atributos em um único (neste caso o "S"), facilitando a exportacao e posterior importacao
+    Square,
+    Input,
+    InputAdress,
+    Neighborhood,
+    UF,
+    Button,
+    Title,
+} 
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Square} from './Board.elements';
+import {S} from './Board.elements'; //todos os estilos estão declarados dentro de "S" desta forma basta digitar S(ponto) onde sria uma div, sectio, paragrafo, etc...
 
 const Board = () => {
     return (
         <div>
-            <Square>Test</Square>
+            <S.Input placeholder="Insira o CEP"></S.Input>
+            <S.Button>Pesquisar</S.Button>{/* todos os atributos do botao que estao em "elements" foram aplicados aqui usando-se a tag determinada com seu nome */}
+            <S.Square>
+                <div>
+                    <S.Title>Cidade</S.Title>
+                    <S.InputAdress ></S.InputAdress>
+                    <S.Title>Endereço</S.Title>
+                    <S.InputAdress ></S.InputAdress>
+                    <S.Title>Complemento</S.Title>
+                    <S.InputAdress ></S.InputAdress>
+                </div>
+                <div>
+                    <S.Title>Bairro</S.Title>
+                    <S.Neighborhood ></S.Neighborhood>
+                    <S.Title>UF</S.Title>
+                    <S.UF ></S.UF>
+                </div>
+            </S.Square>
         </div>
     );
 };
